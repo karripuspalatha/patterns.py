df89fac68785b444b0a91096c5396509158e99a4
karripuspalatha <karripuspalatha2005@gmail.com>
2026-07-14 11:51:09 +0530

    Add files via upload

---
+++ parttens.py
@@ -0,0 +1,110 @@
+rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 3 or col == 0 or col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 6 or col == 0 or col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 3 or row == 6 or col == 0 or col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 6 or col == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 6 or col == 0 or col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 3 or row == 6 or col == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 3 or col == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or row == 6 or col == 0 or row == 3 or col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if col == 0 or col == 6 or row == 3:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if row == 0 or col == 3 or row == 6 or col == 0:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+    rows = 7
+
+for row in range(rows):
+    for col in range(rows):
+        if col == 0 or row == col or row + col == 6:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
